698107ab7fb240ef20858a92d9fb20e9c3945c80
fred-maina <fchege04@gmail.com>
2024-07-01 12:43:47 +0300

    Add Navbar for Small screens

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
-import logo from '../../assets/MainLogo.png'
-
-
+import logo from '../../assets/MainLogo.png';
 
 function Nav() {
-  return (
-    <div>
-     <nav className="Nav">
-    <div className='NavLogo'>
-    <img src={logo} alt="Main Logo" />
+  const [isOpen, setIsOpen] = useState(false);
 
-    </div>
-    <div className="NavItems">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#contact">Contact us</a>
-          <button className='signinBtn'>
-            Sign In
-            </button>  
-    </div>
-     </nav>
-    </div>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="Nav">
+      <div className="NavLogo">
+        <img src={logo} alt="Main Logo" />
+      </div>
+      <button className="NavToggler" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <div className={`NavItems ${isOpen ? 'open' : ''}`}>
+        <button className="NavCloseBtn" onClick={toggleMenu}>
+          &times;
+        </button>
+        <a href="#home" onClick={toggleMenu}>Home</a>
+        <a href="#about" onClick={toggleMenu}>About</a>
+        <a href="#contact" onClick={toggleMenu}>Contact us</a>
+        <button className="signinBtn" onClick={toggleMenu}>Sign In</button>
+      </div>
+    </nav>
   );
 }
 
