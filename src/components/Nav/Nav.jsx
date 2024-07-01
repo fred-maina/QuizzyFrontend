8a728ebf6cb9ec05edad3f85f4d231b840ee5996
fred-maina <fchege04@gmail.com>
2024-07-01 12:28:53 +0300

    Finish Nav Component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Nav.css';
+import logo from '../../assets/MainLogo.png'
+
+
 
 function Nav() {
   return (
-    <div className="Nav">
-      NavBar jamani
-      {/* Nav content goes here */}
+    <div>
+     <nav className="Nav">
+    <div className='NavLogo'>
+    <img src={logo} alt="Main Logo" />
+
+    </div>
+    <div className="NavItems">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact us</a>
+          <button className='signinBtn'>
+            Sign In
+            </button>  
+    </div>
+     </nav>
     </div>
   );
 }
