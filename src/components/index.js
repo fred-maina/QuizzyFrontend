2798f0882c5b588a7a9e8f2e94fcfaa3f2b2bf8c
fred-maina <fchege04@gmail.com>
2024-07-13 22:23:22 +0300

    Add feedback Component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header }  from './Header/Header';
 export { default as Card } from './Card/Card';
 export { default as Button } from './Button/Button';
 export { default as Footer } from './Footer/Footer';
+export { default as Feedback} from'./Feedback/Feedback';
