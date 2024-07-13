ab51c918b6e7ce9cc36dbcc9064fdce2a4d3c588
fred-maina <fchege04@gmail.com>
2024-07-13 21:57:59 +0300

    Add jsx code for the cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Card.css';
+import pencil from '../../assets/PencilIcon.png'
+import graph from '../../assets/stats.png'
+import grade from '../../assets/test.png'
+import time from '../../assets/time-forward.png'
 
 function Card() {
   return (
     <div className="Card">
-      {/* Card content goes here */}
+      <h1> Why Quizzy </h1>
+      <div className='whyQuizzy'>
+        <div className='Pencil'>
+          <img src= {pencil} alt="" />
+          <p>Create engaging quizzes effortlessly with our intuitive interface.</p>
+        </div>
+        <div className='Graph'>
+          <img src={graph} alt="" />
+          <p>Gain valuable insights on quiz performance and user engagement."</p>
+        </div>
+        <div className='Time'>
+          <img src={grade} alt="" />
+          <p>Save time with automated grading and instant feedback</p>
+        </div>
+        <div className='time'>
+          <img src={time} alt="" />
+          <p>Get real-time results and track performance instantly.</p>
+
+        </div>
+      </div>
     </div>
   );
 }
