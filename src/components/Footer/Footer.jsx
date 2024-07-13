e32ad17589c79f0d5de9eb8f9a5c6b9d522fedc8
fred-maina <fchege04@gmail.com>
2024-07-14 00:02:09 +0300

    Add Footer JSx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import './Footer.css';
+import './Footer.css'; // Import your CSS file
 
-function Footer() {
-  return (
-    <div className="Footer">
-      {/* Footer content goes here */}
-    </div>
-  );
-}
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>
+                Designed and developed by 
+                <a href="https://github.com/fred-maina" target="_blank" rel="noopener noreferrer"> Fredrick Maina </a>
+                <br />
+                © 2024 All rights reserved 
+                <br />
+                Contact: +254743039297
+            </p>
+        </footer>
+    );
+};
 
 export default Footer;
