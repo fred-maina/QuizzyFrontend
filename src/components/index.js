c5e070453970673118eda41350c917a27b682940
fred-maina <fchege04@gmail.com>
2024-07-14 19:47:20 +0300

    Add Login form

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Nav } from './Nav/Nav';
 export { default as Header }  from './Header/Header';
 export { default as Card } from './Card/Card';
-export { default as Button } from './Button/Button';
+export { default as Form } from './Form/Form';
 export { default as Footer } from './Footer/Footer';
 export { default as Feedback} from'./Feedback/Feedback';
