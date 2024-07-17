19a81279e040b8d403f5b109e359886f91dc2b51
fred-maina <fchege04@gmail.com>
2024-07-17 22:06:59 +0300

    Add sidebar jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Sidebar.css';
-
+import Profile from '../../assets/Dashboard/account.png'
+import Graph from '../../assets/Dashboard/data-analytics.png'
+import Pencil from '../../assets/Dashboard/edit.png'
+import Notification from '../../assets/Dashboard/bell.png'
+import Add from '../../assets/Dashboard/add.png'
 const Sidebar = () => {
   return (
     <div className="Sidebar">
-      <h1>Sidebar Component</h1>
+      <div className='Sidebar_item Analytics'>
+        <img src={Graph} alt="" />
+        <label htmlFor=""> Analytics</label>
+      </div>
+     
+      <div className='Sidebar_item TakeQuiz'>
+        <img src={Pencil} alt="" />
+        <label htmlFor=""> Take a Quiz</label>
+      </div>
+      <div className='Sidebar_item Add'>
+        <img src={Add} alt="" />
+        <label htmlFor=""> Create New Quiz</label>
+      </div>
+      <div className='Sidebar_item Notifications'>
+        <img src={Notification} alt="" />
+        <label htmlFor=""> Notifications</label>
+      </div>
+      <div className='Sidebar_item Profile'>
+        <img src={Profile} alt="" />
+        <label htmlFor=""> Profile</label>
+      </div>
+      
     </div>
   );
 };
