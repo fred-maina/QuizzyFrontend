5e5198fe413b59b45c18af498837c31dc7bf0f9b
fred-maina <fchege04@gmail.com>
2024-07-17 22:07:30 +0300

    Add Sidebar to Dashboard component

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Dashboard.css';
+import { Sidebar, TakeQuiz, Profile, Notification, CreateQuiz,Analytics } from '../../components';
 
 function Dashboard() {
   return (
     <div className="Dashboard">
+      
+      <Sidebar />
     
     </div>
   );
