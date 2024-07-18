0e4ef50f2974b54f33d4ddad4f1c58c6c0bb0ca3
fred-maina <fchege04@gmail.com>
2024-07-18 11:57:55 +0300

    Change Navigation of the dashboard

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar.jsx';
+import { Analytics, CreateQuiz, Notification, Profile, TakeQuiz } from '../../components/index.js';
 import './Dashboard.css';
-import { Sidebar, TakeQuiz, Profile, Notification, CreateQuiz,Analytics } from '../../components';
 
-function Dashboard() {
+const Dashboard = () => {
+  const [selectedItem, setSelectedItem] = useState('Analytics');
+
+  const renderContent = () => {
+    switch (selectedItem) {
+      case 'Analytics':
+        return <Analytics />;
+      case 'TakeQuiz':
+        return <TakeQuiz />;
+      case 'CreateQuiz':
+        return <CreateQuiz />;
+      case 'Notifications':
+        return <Notification />;
+      case 'Profile':
+        return <Profile />;
+      default:
+        return <Analytics />;
+    }
+  };
+
   return (
     <div className="Dashboard">
-      
-      <Sidebar />
-    
+      <Sidebar className="Sidebar" onSelectItem={setSelectedItem} />
+      <div className="content">
+        {renderContent()}
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
