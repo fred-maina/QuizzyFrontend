882e90b1082d8087e4814a0a2fad8b95a16da14a
fred-maina <fchege04@gmail.com>
2024-08-29 20:26:30 +0300

    export the not found component

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,3 +2,4 @@
 export { default as LandingPage } from './LandingPage/LandingPage';
 export { default as Dashboard } from './Dashboard/Dashboard';
 export { default as SignInSignUp } from './SignInSignUp/SignInSignUp';
+export {default as NotFound} from './NotFound/NotFound';
